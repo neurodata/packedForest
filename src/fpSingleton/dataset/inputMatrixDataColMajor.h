821d8d36832fdffc0633fcac1f59c67650b43bf7
Ben Falk <falk.ben@gmail.com>
2019-02-18 16:07:57 -0500

    Inherit row col matrix input (#11)
    * making the row/col major matrix loads inherit from base class
    
    * add constructor with inputs for subclasses row/col major
    
    * changes in order to get it to compile
    
    * replace getter with direct call to attributes

--- src/fpSingleton/dataset/inputMatrixDataColMajor.h
+++ src/fpSingleton/dataset/inputMatrixDataColMajor.h
@@ -1,95 +1,31 @@
 #ifndef inputMatrixDataColMajor_h
 #define inputMatrixDataColMajor_h
-#include <vector>
-#include <iostream>
-#include "inputData.h"
-#include <string>
 
+#include "inputMatrixData.h"
 
-namespace fp {
+namespace fp
+{
 
-	template <typename T, typename Q>
-		class inputMatrixDataColMajor : public inputData<T,Q>
+template <typename T, typename Q>
+class inputMatrixDataColMajor : public inputMatrixData<T, Q>
+{
+  public:
+	inputMatrixDataColMajor(const T *Xmat, const Q *Yvec, int numObs, int numFeatures) : inputMatrixData<T, Q>(Xmat, Yvec, numObs, numFeatures)
 	{
-		private:
-			const T* inputXData;
-			const Q* inputYData;
-			int numClasses;
-			int numObs;
-			int numFeatures;
-		public:
-			inputMatrixDataColMajor( const T* Xmat, const Q* Yvec, int numObs, int numFeatures):inputXData(Xmat),inputYData(Yvec),numClasses(-1),numObs(numObs),numFeatures(numFeatures){
-				countAndCheckClasses();
-			}
+	}
 
-			~inputMatrixDataColMajor(){
-				//TODO destroy X and Y
-			}
+	inline T returnFeatureValue(const int &featureNum,
+								const int &observationNum)
+	{
+		return this->inputXData[this->numObs * featureNum + observationNum];
+	}
 
-			inline Q returnClassOfObservation(const int &observationNum){
-				return inputYData[observationNum];
-			}
+	inline void prefetchFeatureValue(const int &featureNum,
+									 const int &observationNum)
+	{
+		__builtin_prefetch(&this->inputXData[this->numObs * featureNum + observationNum], 0, 2);
+	}
+};
 
-			inline T returnFeatureValue(const int &featureNum, const int &observationNum){
-				return inputXData[numObs*featureNum + observationNum];
-			}
-
-			inline void prefetchFeatureValue(const int &featureNum, const int &observationNum){
-				__builtin_prefetch(&inputXData[numObs*featureNum + observationNum], 0, 2);
-			}
-
-			inline int returnNumFeatures(){
-				return numFeatures;
-			}
-
-			inline int returnNumObservations(){
-				return numObs;
-			}
-
-			inline int returnNumClasses(){
-				return numClasses;
-			}
-
-
-			inline void countAndCheckClasses(){
-				std::vector<short> classRoll;
-				for(int i = 0; i < numObs;++i){
-					if(inputYData[i] >= numClasses){
-						numClasses=inputYData[i]+1;
-						classRoll.resize(numClasses,0);
-					}
-					classRoll[inputYData[i]]=1;
-				}
-
-				for(auto i : classRoll){
-					if(i != 1){
-						throw std::runtime_error("Not all classes represented in input." );
-					}
-				}
-			}
-
-
-			void printDataStats(){
-				std::cout << "there are " << returnNumFeatures() << " features.\n";
-				std::cout << "there are " << returnNumObservations() << " observations.\n";
-			}
-
-			void printXValues(){
-				for(int i = 0; i < returnNumFeatures(); i++){
-					for(int j = 0; j < returnNumObservations(); j++){
-						std::cout << returnFeatureValue(i,j) << " ";
-					}
-					std::cout << "\n";
-				}
-			}
-
-			void printYValues(){
-				for(int j = 0; j < returnNumObservations(); j++){
-					std::cout << returnClassOfObservation(j) << " ";
-				}
-				std::cout << "\n";
-			}
-	};
-
-} //namespace fp
-#endif //inputMatrixDataColMajor_h
+} // namespace fp
+#endif // inputMatrixDataColMajor_h
